90b12871171f10fc29c48c77828b778e410dc13b
Pedro Saravia <pedro.saravia.q@uni.pe>
2024-10-25 21:49:49 -0500

    add vite command

--- javascript/reactjs/src/main.tsx
+++ javascript/reactjs/src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 
+//~ npm create vite@latest
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
