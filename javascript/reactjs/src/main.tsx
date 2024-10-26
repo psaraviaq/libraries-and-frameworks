3c7aa15ccc4548c92ce15a425cd768d3ffd98986
Pedro Saravia <pedro.saravia.q@uni.pe>
2024-10-25 22:57:28 -0500

    add custom commands

--- javascript/reactjs/src/main.tsx
+++ javascript/reactjs/src/main.tsx
@@ -5,6 +5,11 @@ import "./index.css";
 
 //~ npm create vite@latest
 
+//^ Custom commands
+//* Alt + O: Emmet Balance Outward
+//* Alt + I: Emmet Balance Inward
+//* Alt + A: Emmet Wrap with Abbreviation
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
