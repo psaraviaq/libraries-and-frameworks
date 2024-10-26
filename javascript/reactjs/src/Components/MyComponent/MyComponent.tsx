bbbe3e2a6ab695a196d035cd71877b6bdcbc21d2
Pedro Saravia <pedro.saravia.q@uni.pe>
2024-10-26 12:32:23 -0500

    add component with props

--- javascript/reactjs/src/Components/MyComponent/MyComponent.tsx
+++ javascript/reactjs/src/Components/MyComponent/MyComponent.tsx
@@ -4,3 +4,8 @@ const MyComponent = () => {
 };
 
 export default MyComponent;
+
+//* Component with props
+//! We're not going to use them frequently
+// const MyComponent : React.FC<Props> = ({prop1, prop2}) => {}
+// const MyComponent = ({prop1, prop2}: Props) : JSX.Element => {}
